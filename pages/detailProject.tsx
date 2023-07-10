5e803c87efbd9a0a956f5b9bb3513297b9b8def5
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-07-10 18:46:21 +0700

    .

--- pages/detailProject.tsx
+++ pages/detailProject.tsx
@@ -1,7 +1,10 @@
-import React , {useState} from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
 import ProjectData from "../data/DataProject";
 import { useRouter } from "next/router";
+import { BiLeftArrowAlt } from "react-icons/bi";
 
 export default function DetailProject() {
   const router = useRouter();
@@ -16,28 +19,45 @@ export default function DetailProject() {
   }
 
   const { image, title, hastag, description, thumbnail } = project;
+  const [selectedImage, setSelectedImage] = useState(image);
+
+  const handleThumbnailClick = (thumbnailImage: any) => {
+    setSelectedImage(thumbnailImage);
+  };
 
   return (
     <div className="max-w-screen-xl px-4 py-10  mx-auto">
-      <h1 className="font-bold text-red-600 text-2xl mb-5">Project {id}</h1>
-      <Image
-        src={image}
-        alt={title}
-        className="w-full rounded-md border border-gray-700"
-        width={1000}
-        height={1000}
-      />
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 mt-3 gap-2 justify-between ">
-        {thumbnail.map((thumb, index) => (
-          <Image
-            key={index}
-            src={thumb}
-            alt={`Thumbnail ${index + 1}`}
-            width={300}
-            height={300}
-            className="rounded-md  border-gray-700 border w-full"
-          />
-        ))}
+      <Link
+        href="/project"
+        className="rounded-md bg-red-600 text-white p-2.5 w-20  mb-10 flex items-center"
+      >
+        <BiLeftArrowAlt className="text-2xl" />
+        <h1>Back</h1>
+      </Link>
+
+      <div className="lg:flex">
+        <Image
+          src={selectedImage}
+          alt={title}
+          className="w-full h-auto rounded-md border border-gray-700"
+          width={700}
+          height={700}
+        />
+        <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-2 lg:ms-3 gap-2 lg:mt-0 mt-3 w-full justify-between ">
+          {thumbnail.map((thumb, index) => (
+            <Image
+              key={index}
+              src={thumb}
+              alt={`Thumbnail ${index + 1}`}
+              width={300}
+              height={300}
+              className={`rounded-md border-gray-700 border cursor-pointer w-full  ${
+                selectedImage === thumb ? "border-red-600" : ""
+              }`}
+              onClick={() => handleThumbnailClick(thumb)}
+            />
+          ))}
+        </div>
       </div>
       <h2 className="mt-5 text-red-600 font-bold text-2xl">{title}</h2>
       <figcaption className="text-gray-700 mb-5 ">
